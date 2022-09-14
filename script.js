259022e9ed5aae76b709f6e9af61e9425a60c5bf
aoespalza <aoespalza@hotmail.com>
2022-09-14 17:20:06 -0500

    Primer Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const h1= document.querySelector('h1');
+const input1= document.querySelector('#calculo1');
+const input2= document.querySelector('#calculo2');
+const btn= document.querySelector('#btnCalcular');
+const pSuma= document.querySelector('#result');
+btn.addEventListener('click', btnOnClick);
+
+function btnOnClick() {
+    const sumaInputs = parseInt(input1.value) + parseInt(input2.value);
+    pSuma.innerText = "Resultado " +  sumaInputs;
+    h1.innerHTML= "Andrés es Hermoso"
+	
+}
